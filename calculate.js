c2d0f7ec26b6154ddadbfe9c290b3ee452159be4
Neul <neulseo420@gmail.com>
2021-06-29 15:51:44 -0400

    Add window.alert

--- calculate.js
+++ calculate.js
@@ -8,9 +8,9 @@ function process() {
     const output = document.querySelector('#output');
 
     // Declare variables for the output:
-    const message = '';
-    const interval = '';
-    const day = 1000 * 60 * 60 * 24; // one day in milliseconds
+    let message = '';
+    let interval = '';
+    let day = 1000 * 60 * 60 * 24; // one day in milliseconds
 
     // Create new Date() objects for starting and ending dates:
     const startDate = new Date(start.value);
@@ -20,22 +20,22 @@ function process() {
     if (startDate.getTime() && endDate.getTime()) {
         // Make sure the start date comes first:
         if (startDate < endDate) { // ---> if the startDate is before endDate
-
-            const diff = endDate - startDate;
+            let diff = endDate - startDate;
             if (diff <= day) { // ---> if the difference is less than or equal to a day
                 interval = '1 day';
             } else {
-                interval = `${Math.round(diff / day)} days`; // ---> if the difference is over a day
+                interval = Math.round(diff / day) + ' days'; // ---> if the difference is over a day
             }
-
-            message = `Start Date: ${startDate.toLocalDateString()}, `;
-            message += `End Date: ${endDate.toLocaleDateString()}, `;
+            message = `Start Date: ${startDate.toLocaleDateString()},\n`;
+            message += `End Date: ${endDate.toLocaleDateString()},\n`;
             message += `Duration: ${interval}.`;
         } else {
             message = `The start date must come before the end date!`;
+            window.alert(message);
         }
     } else {
         message = `Please enter valid start and end dates in the format MM/DD/YYYY`;
+        window.alert(message);
     }
 
     // Update the page with the custom message:
