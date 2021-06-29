194727aeabb1839c3596fcb387c907a3681f1255
Neul <neulseo420@gmail.com>
2021-06-29 15:00:38 -0400

    Write steps for calculate.js

---
+++ calculate.js
@@ -0,0 +1,21 @@
+/* calculate.js */
+function process() {
+    'use strict';
+
+    // Get reference to the HTML elements:
+
+    // Declare variables for the output:
+
+    // Create new Date() objects for starting and ending dates:
+
+    // Confirm that the input dates are valid:
+
+    // Update the page with the custom message:
+
+} // end of process()
+
+function init() {
+    'use strict';
+
+} // end of init()
+window.onload = init;
