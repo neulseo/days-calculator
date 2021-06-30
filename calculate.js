a3bf1f09b0e6a289276f6e8c5e5c34143aa4ba0d
Neul <neulseo420@gmail.com>
2021-06-30 11:30:03 -0400

    Update days-caculator.html
    leave a note

--- calculate.js
+++ calculate.js
@@ -6,32 +6,43 @@ function process() {
     const start = document.querySelector('#start');
     const end = document.querySelector('#end');
     const output = document.querySelector('#output');
+    const duration = document.querySelector('#duration');
 
     // Declare variables for the output:
     let message = '';
     let interval = '';
     let day = 1000 * 60 * 60 * 24; // one day in milliseconds
 
+    
     // Create new Date() objects for starting and ending dates:
     const startDate = new Date(start.value);
     const endDate = new Date(end.value);
-
+    
+    let diff = endDate - startDate;
+    if (diff <= day) { // ---> if the difference is less than or equal to a day
+        interval = '1 day';
+    } else {
+        interval = Math.round(diff / day) + ' days'; // ---> if the difference is over a day
+    }
+    
     // Confirm that the input dates are valid:
     if (startDate.getTime() && endDate.getTime()) {
         // Make sure the start date comes first:
         if (startDate < endDate) { // ---> if the startDate is before endDate
-            let diff = endDate - startDate;
-            if (diff <= day) { // ---> if the difference is less than or equal to a day
-                interval = '1 day';
-            } else {
-                interval = Math.round(diff / day) + ' days'; // ---> if the difference is over a day
-            }
+            // let diff = endDate - startDate;
+            // if (diff <= day) { // ---> if the difference is less than or equal to a day
+            //     interval = '1 day';
+            // } else {
+            //     interval = Math.round(diff / day) + ' days'; // ---> if the difference is over a day
+            // }
             message = `Start Date: ${startDate.toLocaleDateString()},\n`;
             message += `End Date: ${endDate.toLocaleDateString()},\n`;
             message += `Duration: ${interval}.`;
         } else {
-            message = `The start date must come before the end date!`;
-            window.alert(message);
+            // message = `The start date must come before the end date!`;
+            // window.alert(message);
+            const text = interval.value;
+            console.log(text);
         }
     } else {
         message = `Please enter valid start and end dates in the format MM/DD/YYYY`;
